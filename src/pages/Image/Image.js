60d3c313121b721efa9bf6daecb8574f782f27d6
Nikodem <nikodem.lorenz@gmail.com>
2019-07-21 20:32:56 +0200

    Added a fallback if image owner has no profile picture; Added 'favorites' button

--- src/pages/Image/Image.js
+++ src/pages/Image/Image.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react'
 import {connect} from 'react-redux'
 import styles from './Image.module.scss'
-import {MdFileDownload, MdThumbUp, MdRemoveRedEye} from 'react-icons/md'
+import {MdFileDownload, MdThumbUp, MdRemoveRedEye, MdAddBox} from 'react-icons/md'
 
 const Image = props =>{
+
+   const [hasNoProfileImage, setHasNoProfileImage] = useState(false)
 
    if(props.images.length === 0) return null
 
@@ -24,15 +26,29 @@ const Image = props =>{
          </section>
 
          <section className={styles.right}>
+
             <div className={styles.author}>
                <h3>By {img.user}</h3>
-               <img src={img.userImageURL} alt="userImage"/>
+
+               {
+                  hasNoProfileImage
+                     ? null
+                     : <img src={img.userImageURL} alt="userImage" onError={() => setHasNoProfileImage(true)}/>
+               }
             </div>
 
-            <a download="filename.jpg" target="_blank" href={img.largeImageURL}>
-               Click here to get this image
-               <MdFileDownload/>
-            </a>
+            <div className={styles.buttons}>
+               <h3 className={styles.favorites}>
+                  Add to favorites
+                  <MdAddBox/>
+               </h3>
+
+               <a download="filename.jpg" target="_blank" href={img.largeImageURL}>
+                  Download Picture
+                  <MdFileDownload/>
+               </a>
+            </div>
+
          </section>
       </div>
    )
