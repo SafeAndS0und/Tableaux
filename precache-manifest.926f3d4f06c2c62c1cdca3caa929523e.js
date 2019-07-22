b5557ab28e870d0102030bd4ed7fd6c524d586c9
Nikodem <nikodem.lorenz@gmail.com>
2019-07-22 14:47:17 +0200

    Updates

---
+++ precache-manifest.926f3d4f06c2c62c1cdca3caa929523e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca38d358ccd0a82583555d9946e73873",
+    "url": "/Tableaux/index.html"
+  },
+  {
+    "revision": "3a925b205e58ea3bc0bb",
+    "url": "/Tableaux/static/css/main.6e8e16b9.chunk.css"
+  },
+  {
+    "revision": "4da54f9f104de85bc1eb",
+    "url": "/Tableaux/static/js/2.eb9e08da.chunk.js"
+  },
+  {
+    "revision": "3a925b205e58ea3bc0bb",
+    "url": "/Tableaux/static/js/main.56599815.chunk.js"
+  },
+  {
+    "revision": "7e1f85ff375d2100fca8",
+    "url": "/Tableaux/static/js/runtime~main.ea51f3e7.js"
+  }
+]);
